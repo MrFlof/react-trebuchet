87b342ca7a1b7bc92ede07aa576d994dd608b913
Barry Staes <barry.staes@q-lite.com>
2016-01-14 12:52:18 +0100

    Added MainMenu component

---
+++ src/components/MainMenu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class MainMenu extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>Menu:</li>
+        <li><Link to="/">Root</Link></li>
+        <li><Link to="/fuelsavings">Fuel Savings</Link></li>
+        <li><Link to="/foo">Foo</Link></li>
+        <li><Link to="/bar">Bar</Link></li>
+        <li><Link to="/planetpluto">Planet Pluto</Link></li>{/* example for NotFoundPage */}
+      </ul>
+    );
+  }
+}
+
+export default MainMenu;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,10 +2,13 @@
 // to support hot reloading in Redux
 import React from 'react';
 
+import MainMenu from '../components/MainMenu';
+
 class App extends React.Component {
   render() {
     return (
       <div>
+        <MainMenu />
         {this.props.children}
       </div>
     );
