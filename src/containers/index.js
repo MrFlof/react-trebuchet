f34c3ca435e7829c1575c394eff3bd45bcdc8d18
Barry Staes <barry.staes@q-lite.com>
2016-01-18 16:09:52 +0100

    Import pages on one line.

---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+const App = require('./App');
+const BarPage = require('./BarPage');
+const FooPage = require('./FooPage');
+const FuelSavingsPage = require('./FuelSavingsPage');
+const NotFoundPage = require('./NotFoundPage');
+
+module.exports = { App, BarPage, FooPage, FuelSavingsPage, NotFoundPage };
