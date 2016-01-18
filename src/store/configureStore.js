973bcd8ee5030f8d21ab2f0d071e4ec00b95c120
Barry Staes <barry.staes@q-lite.com>
2016-01-18 10:40:00 +0100

    Updated to redux-simple-router v2.0.3

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,22 @@
 //This boilerplate file is likely to be the same for each project that uses Redux.
 //With Redux, the actual stores are in /reducers.
 
-import { createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import rootReducer from '../reducers';
+import { createHistory } from 'history';
+import { syncHistory } from 'redux-simple-router';
+
+const history = createHistory();
+const storemiddlewareHistory = syncHistory(history);
+
+const finalCreateStore = compose(
+  applyMiddleware(storemiddlewareHistory)
+)(createStore);
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const store = finalCreateStore(rootReducer, initialState);
+
+  storemiddlewareHistory.listenForReplays(store);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
