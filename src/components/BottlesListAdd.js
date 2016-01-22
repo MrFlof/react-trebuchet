7a5cc128bd526dd7280385d2208d91d24bfdbffa
Barry Staes <barry.staes@q-lite.com>
2016-01-22 14:57:07 +0100

    Now ADD_BOTTLE action processes all fields.

--- src/components/BottlesListAdd.js
+++ src/components/BottlesListAdd.js
@@ -3,55 +3,57 @@ import React, { PropTypes } from 'react';
 class BottlesListAdd extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: "default name",
+    this.state = this.initialState();
+    // Manually bind this method to the component instance...
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  initialState() {
+    return {
+      name: "Bla",
       brand: "",
       type: "",
       country: "",
-      contents_cl: 0,
+      contents_cl: null,
       alcohol_pct: null
     };
-    // Manually bind this method to the component instance...
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleBrandChange = this.handleBrandChange.bind(this);
   }
 
   handleAdd(e) {
     e.preventDefault();
-    var { name, brand } = this.state;
+    var { name, brand, type, country, contents_cl, alcohol_pct } = this.state;
     this.props.onAdd({
       name: name,
       brand: brand,
-      type: "Melk",
-      country: "Scheutland",
-      contents_cl: 50,
-      alcohol_pct: null
+      type: type,
+      country: country,
+      contents_cl: contents_cl,
+      alcohol_pct: alcohol_pct
     });
+    this.setState(this.initialState()); // reset UI
   }
 
-  handleNameChange(e) {
-    this.setState({name: e.target.value});
-  }
-
-  handleBrandChange(e) {
-    this.setState({brand: e.target.value});
+  handleChange(key, e) {
+    var change = {};
+    change[key] = e.target.value;
+    this.setState(change);
   }
 
   render() {
-    var { name, brand } = this.state;
+    var { name, brand, type, country, contents_cl, alcohol_pct } = this.state;
     return (
       <form onSubmit={this.handleAdd}>
         <table>
           <tbody>
             <tr>
               <td>New:</td>
-              <td><input className="small" type="text" name="name" onChange={this.handleNameChange} value={name} /></td>
-              <td><input className="small" type="text" name="brand" onChange={this.handleBrandChange} value={brand} /></td>
-              <td><input className="small" type="text" name="type" /></td>
-              <td><input className="small" type="text" name="country" /></td>
-              <td><input className="small" type="number" name="contents_cl" /></td>
-              <td><input className="small" type="number" name="alcohol_pct" step="0.5" /></td>
+              <td><input className="small" type="text" name="name" value={name} onChange={this.handleChange.bind(this, "name")} /></td>
+              <td><input className="small" type="text" name="brand" value={brand} onChange={this.handleChange.bind(this, "brand")} /></td>
+              <td><input className="small" type="text" name="type" value={type} onChange={this.handleChange.bind(this, "type")} /></td>
+              <td><input className="small" type="text" name="country" value={country} onChange={this.handleChange.bind(this, "country")} /></td>
+              <td><input className="small" type="number" name="contents_cl" value={contents_cl} onChange={this.handleChange.bind(this, "contents_cl")} /></td>
+              <td><input className="small" type="number" name="alcohol_pct" step="0.5" value={alcohol_pct} onChange={this.handleChange.bind(this, "alcohol_pct")} /></td>
               <td><input className="small" type="submit" value="Add" /></td>
             </tr>
           </tbody>
