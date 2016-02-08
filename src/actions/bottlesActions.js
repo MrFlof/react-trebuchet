cfb153c12695ce4a07c4d5d291ae7f26665d611c
Barry Staes <barry.staes@q-lite.com>
2016-02-08 16:18:20 +0100

    Applied normalizr to bottles. (todo apply to collection as well)

--- src/actions/bottlesActions.js
+++ src/actions/bottlesActions.js
@@ -1,6 +1,10 @@
 import { CALL_API } from 'redux-api-middleware';
+import { Schema, arrayOf, normalize } from 'normalizr';
 
 import * as types from '../constants/ActionTypes';
+
+
+const bottleSchema = new Schema('bottles');
 
 
 export function addBottle(bottle) {
@@ -39,7 +43,20 @@ export function fetchCollection(collectionId) {
     [CALL_API]: {
       endpoint: 'http://10.13.0.125:3030/collections/1/?_embed=bottles',
       method: 'GET',
-      types: ['FETCHCOLLECTION_REQUEST', 'FETCHCOLLECTION_SUCCESS', 'FETCHCOLLECTION_FAILURE']
+      types: [
+        'FETCHCOLLECTION_REQUEST',
+        {
+          type: 'FETCHCOLLECTION_SUCCESS',
+            payload: (action, state, res) => {
+            const contentType = res.headers.get('Content-Type');
+            if (contentType && ~contentType.indexOf('json')) {
+              // Just making sure res.json() does not raise an error
+              return res.json().then((json) => normalize(json, { bottles: arrayOf(bottleSchema) }));
+            }
+          }
+        },
+        'FETCHCOLLECTION_FAILURE'
+      ]
     }
   };
   // a SUCCESS results in Normalizr using the bottles:[]
