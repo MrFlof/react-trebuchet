17068ac82e4f0778172575ab3048330f625ca194
Barry Staes <barry.staes@q-lite.com>
2016-02-22 11:31:36 +0100

    Fix #14 with router history push, added example.

--- src/index.js
+++ src/index.js
@@ -4,21 +4,19 @@ import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 
-import { Router, Route } from 'react-router';
-import { createHistory } from 'history';
+import { Router, Route, browserHistory } from 'react-router';
 import { syncHistory } from 'react-router-redux';
 import { createStore } from 'redux';
 
 import * as Containers from './containers';
 
 
-const history = createHistory();
-const storemiddlewareHistory = syncHistory(history);
+const storemiddlewareHistory = syncHistory(browserHistory);
 const store = configureStore(undefined, storemiddlewareHistory);
 
 render(
   <Provider store={store}>
-    <Router history={history}>
+    <Router history={browserHistory}>
       <Route path="/" component={Containers.App}>
         <Route path="fuelsavings" component={Containers.FuelSavingsPage}/>
         <Route path="simplecounter" component={Containers.SimpleCounterPage}/>
